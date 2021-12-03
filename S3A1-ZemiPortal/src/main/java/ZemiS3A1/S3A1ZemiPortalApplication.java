ff3575ece1b8df3cdcb41af9b259406c8890cdaf
kou06 <kou06@LAPTOP-3P6ODF5N>
2021-12-03 11:28:24 +0900

    aaaa

---
+++ S3A1-ZemiPortal/src/main/java/ZemiS3A1/S3A1ZemiPortalApplication.java
@@ -0,0 +1,13 @@
+package ZemiS3A1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3A1ZemiPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3A1ZemiPortalApplication.class, args);
+	}
+
+}
---
+++ S3A1-ZemiPortal/src/test/java/ZemiS3A1/S3A1ZemiPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package ZemiS3A1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3A1ZemiPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
