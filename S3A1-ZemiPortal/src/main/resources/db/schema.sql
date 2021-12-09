6d9786fc1b9a76a4696a386b87f50fc2c848a211
s20193005 <s20193005@HCS54M142.stdlan.hcs.local>
2021-12-09 10:22:09 +0900

    データベースの作成

--- S3A1-ZemiPortal/src/main/resources/db/schema.sql
+++ S3A1-ZemiPortal/src/main/resources/db/schema.sql
@@ -1,24 +1,40 @@
 /* 開発用にデータ削除を追加 : リリース時は消す
 DROP TABLE m_user;
-DROP TABLE task;
- */
+DROP TABLE card;
+DROP TABLE list;
+*/
 
 /* ユーザマスタ */
 CREATE TABLE IF NOT EXISTS m_user (
     user_id VARCHAR(50) PRIMARY KEY,
-    encrypted_password VARCHAR(100),
-    user_name VARCHAR(50),
-    darkmode BOOLEAN,
-    role VARCHAR(50),
+    encrypted_password VARCHAR(100) NOT NULL,
+    user_name VARCHAR(50) NOT NULL,
+    user_role VARCHAR(50) NOT NULL,
+    user_status VARCHAR(7) NOT NULL,
+    created_at TIMESTAMP,
+    registeredperson_id VARCHAR(254),
+    updated_at TIMESTAMP,
+    changer_id VARCHAR(254),
     enabled BOOLEAN
 );
 
-/* タスクテーブル */
-CREATE TABLE IF NOT EXISTS task (
-  id INT PRIMARY KEY,
-  user_id VARCHAR(50),
-  priority VARCHAR(10),
-  title VARCHAR(50),
-  comment VARCHAR(200),
-  limitday DATE
+/*カードテーブル*/
+CREATE TABLE IF NOT EXISTS card (
+ card_id VARCHAR(254) PRIMARY KEY,
+ card_title VARCHAR(100),
+ card_detail VARCHAR(254),
+ user_id VARCHAR(60) REFERENCES m_user (user_id),
+ list_id VARCHAR(254) REFERENCES list (list_id),
+ card_date TIMESTAMP,
+ card_check CHAR(5),
+ card_description VARCHAR(254),
+ card_detail_description VARCHAR(254)
+);
+
+/*リストテーブル*/
+CREATE TABLE IF NOT EXISTS list (
+ list_id VARCHAR(254) PRIMARY KEY,
+ list_title VARCHAR(100),
+ user_id VARCHAR(60) REFERENCES m_user (user_id),
+ card_id VARCHAR(254) REFERENCES card (card_id)
 );
