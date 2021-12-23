7199a417d000c004264fa0d6f9aeb838b3f29a1c
kou06 <kou06@LAPTOP-3P6ODF5N>
2021-12-24 02:32:48 +0900

    カードの追加機能のモーダルウィンドウを側のみ追加

--- S3A1-ZemiPortal/src/main/resources/static/js/index.js
+++ S3A1-ZemiPortal/src/main/resources/static/js/index.js
@@ -1,4 +1,4 @@
-$('#testModal3').on('show.bs.modal', function (event) {
+$('#card-addition').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
   var recipient = button.data('whatever') // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
